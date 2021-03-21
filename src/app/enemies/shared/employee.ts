6d2d6fbefbd5663584358f52ed9df354b7d6313a
Don Carlos Zendejas <czende@hotmail.com>
2021-03-20 20:26:44 -0600

    Update employee.ts

--- src/app/enemies/shared/employee.ts
+++ src/app/enemies/shared/employee.ts
@@ -3,4 +3,10 @@ export class Employee {
    name: string;
    email: string;
    phone: number;
+   shape: string;
+   baseMining: number;
+   baseCrystal: number;
+   baseHabitable: number;
+   baseRobot: number;
+   level: number;
 }
