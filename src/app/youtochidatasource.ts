92620b5c43d8bf242875b5055e39fb0491824bf1
Don Carlos Zendejas <czende@hotmail.com>
2021-04-18 18:29:23 -0500

    Create youtochidatasource.ts

---
+++ src/app/youtochidatasource.ts
@@ -0,0 +1,12 @@
+export class Youtochidatasource {
+   id: number;
+   name: string;
+   email: string;
+   phone: number;
+   shape: string;
+   baseMining: number;
+   baseCrystal: number;
+   baseHabitable: number;
+   baseRobot: number;
+   level: number;
+}
